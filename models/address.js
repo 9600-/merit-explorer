515d08bafaaa08c13ce23ef2fe212a7acc9a114e
Overcooked Panda <overcookedpanda@gmail.com>
2018-08-30 09:15:23 -0500

    aliases implementation

--- models/address.js
+++ models/address.js
@@ -7,6 +7,7 @@ var AddressSchema = new Schema({
   received: { type: Number, default: 0 },
   sent: { type: Number, default: 0 },
   balance: {type: Number, default: 0},
+  alias: { type: String, unique: true, default: "" },
 }, {id: false});
 
 module.exports = mongoose.model('Address', AddressSchema);
